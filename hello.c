3d7dbc6e0cd8de234b2da98db4b3bb7eb154d5dc
wplay <wangpingyuan@souche.com>
2019-08-24 23:47:34 +0800

    feat: learn 1 - hello world

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * wanna output?
+ * command `gcc hello.c -o hello.out`
+ * default output a.out
+ */
+
+int main ()
+{
+    /* if without \n, will display `hello world! %`, why? */
+    // one line comments
+    printf("hello world! hello, wplay! \n");
+    return 0;
+}
